5888fef6bce441a5274d12c6ca078d7b5723e9c7
Sakil Ahmed <sakilcoder@gmail.com>
2024-01-18 02:32:43 +0600

    buildings added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,36 @@
 const map = L.map('map').setView([44.303163, -78.262237], 17);
 map.options.minZoom = 16;
-var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-  maxZoom: 20,
-  subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-}).addTo(map);
+// var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+//   maxZoom: 20,
+//   subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+// }).addTo(map);
+
+var basemapCarto = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+            attribution: '',
+            maxZoom: 20,
+        });
+        var googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
+            maxZoom: 20,
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+        }).addTo(map);
+        var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+            maxZoom: 20,
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+        });
+        var googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
+            maxZoom: 20,
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+        });
+
+        var basemaps = {
+            "Hybrid": googleHybrid,
+            "Street": googleStreets,
+            "Terrain": googleTerrain,
+            "Carto": basemapCarto,
+
+        };
+        L.control.layers(basemaps).addTo(map);
+        
 
 var parcelStyle = {
   fillColor: '#fae094',

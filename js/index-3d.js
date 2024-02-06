0c08841a72cace23c18c01d34c62f1d2c13e2f33
Sakil Ahmed <sakilcoder@gmail.com>
2024-02-06 23:34:02 +0600

    plots are in 3d

--- js/index-3d.js
+++ js/index-3d.js
@@ -10,6 +10,7 @@ const map = new mapboxgl.Map({
 });
 
 map.on('style.load', () => {
+    
     const layers = map.getStyle().layers;
         const labelLayerId = layers.find(
             (layer) => layer.type === 'symbol' && layer.layout['text-field']
@@ -240,6 +241,31 @@ map.on('style.load', () => {
                 }
             });
 
+            map.addSource('blocks', {
+                type: 'geojson',
+                data: 'data/blocks.json'
+            });
+
+            map.addLayer({
+                'id': 'blocks-line-layer',
+                'type': 'line',
+                'source': 'blocks',
+                'paint': {
+                    'line-width': 3,
+                    'line-color': '#9d9d9d',
+                    'line-dasharray': [2, 2]
+                }
+            });
+
+            map.addLayer({
+                'id': 'blocks-layer',
+                'type': 'fill',
+                'source': 'blocks',
+                'paint': {
+                    'fill-opacity': 0
+                }
+            });
+
             map.addSource('plot-building', {
                 type: 'geojson',
                 data: 'data/plot-building.json'
@@ -247,11 +273,44 @@ map.on('style.load', () => {
 
             map.addLayer({
                 'id': 'plot-building-layer',
-                'type': 'fill',
+                'type': 'fill-extrusion',
                 'source': 'plot-building',
                 'paint': {
-                    'fill-color': '#ff0000',
-                    'fill-opacity': 0.5,
+                    'fill-extrusion-color': '#ff0000',
+                    'fill-extrusion-opacity': 0.5,
+                    'fill-extrusion-height': 3
+                }
+            });
+
+            map.addSource('road-point', {
+                type: 'geojson',
+                data: 'data/road-point.json'
+            });
+
+            map.addLayer({
+                'id': 'road-point-layer',
+                'type': 'symbol',
+                'source': 'road-point',
+                'layout': {
+                    'text-field': ['get', 'name'],
+                    'text-font': ['Open Sans Regular'],
+                    'text-size': 12,
+                }
+            });
+
+            map.addSource('blocks-center', {
+                type: 'geojson',
+                data: 'data/blocks-center.json'
+            });
+
+            map.addLayer({
+                'id': 'blocks-center-layer',
+                'type': 'symbol',
+                'source': 'blocks-center',
+                'layout': {
+                    'text-field': ['get', 'name'],
+                    'text-font': ['Open Sans Regular'],
+                    'text-size': 12,
                 }
             });
 
@@ -265,9 +324,9 @@ map.on('style.load', () => {
                 'type': 'symbol',
                 'source': 'building-centers',
                 'layout': {
-                    'icon-image': ['match', ['get', 'status'], 'Available', 'green-icon', 'red-icon'], // Choose icon based on "status"
-                    'icon-size': 1,
-                    'icon-allow-overlap': true,
+                    // 'icon-image': ['match', ['get', 'status'], 'Available', 'green-icon', 'red-icon'],
+                    // 'icon-size': 1,
+                    // 'icon-allow-overlap': true,
                     'text-field': ['get', 'p_id'],
                     'text-font': ['Open Sans Regular'],
                     'text-size': 12,
@@ -276,9 +335,93 @@ map.on('style.load', () => {
                 }
             });
 
+            map.on('mousemove', 'blocks-layer', function(e) {
+                map.getCanvas().style.cursor = 'pointer';
+
+                var features = e.features;
+                if (!features || features.length === 0) {
+                    return;
+                }
+                
+                var name = features[0].properties.name;
+                var lot_area = features[0].properties.lot_area;
+                var forntage = features[0].properties.forntage;
+                var intend_use = features[0].properties.intend_use;
+                var frontage_info = forntage == "-"? "" : `Frontage: <strong>${forntage}</strong> (m/ft)<br/>`;
+
+                var info = `<h4>${name}</h4>
+                            Lot Area: <strong><em>${lot_area}</strong> (ha/acre)</em><br/>
+                            ${frontage_info}
+                            Intended Use: <strong>${intend_use}</strong><br/>
+                        `;
+                document.getElementById('info').innerHTML=info;
+            });
+            
+            map.on('mouseleave', 'blocks-layer', function(e) {
+                map.getCanvas().style.cursor = '';
+                document.getElementById('info').innerHTML='';
+            });
+
+            map.on('mousemove', 'plot-building-layer', function(e) {
+                map.getCanvas().style.cursor = 'pointer';
+
+                var features = e.features;
+                if (!features || features.length === 0) {
+                    return;
+                }
+                
+                var name = features[0].properties.p_id;
+                var lot_area = features[0].properties.lot_area;
+                var forntage = features[0].properties.forntage;
+                var intend_use = features[0].properties.intend_use;
+                var frontage_info = forntage == "-"? "" : `Frontage: <strong>${forntage}</strong> (m/ft)<br/>`;
+
+                var info = `<h4>Lot ${name}</h4>
+                            Lot Area: <strong><em>${lot_area}</strong> (ha/acre)</em><br/>
+                            ${frontage_info}
+                            Intended Use: <strong>${intend_use}</strong><br/>
+                        `;
+                document.getElementById('info').innerHTML=info;
+            });
+            
+            map.on('mouseleave', 'plot-building-layer', function(e) {
+                map.getCanvas().style.cursor = '';
+                document.getElementById('info').innerHTML='';
+            });
+
+            map.on('mousemove', 'road-layer', function(e) {
+                map.getCanvas().style.cursor = 'pointer';
+
+                var features = e.features;
+                if (!features || features.length === 0) {
+                    return;
+                }
+                
+                var name = features[0].properties.name;
+                if(name!=null){
+                    var lot_area = features[0].properties.lot_area;
+                    var forntage = features[0].properties.forntage;
+                    var intend_use = features[0].properties.intend_use;
+                    var frontage_info = forntage == "-"? "" : `Frontage: <strong>${forntage}</strong> (m/ft)<br/>`;
+    
+                    var info = `<h4>Lot ${name}</h4>
+                                Lot Area: <strong><em>${lot_area}</strong> (ha/acre)</em><br/>
+                                ${frontage_info}
+                                Intended Use: <strong>${intend_use}</strong><br/>
+                            `;
+                    document.getElementById('info').innerHTML=info;
+                }
+                
+            });
+            
+            map.on('mouseleave', 'road-layer', function(e) {
+                map.getCanvas().style.cursor = '';
+                document.getElementById('info').innerHTML='';
+            });
+
 
         });
     });
 });
 
-map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.NavigationControl(), 'top-left');
